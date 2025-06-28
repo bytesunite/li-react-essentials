202afd1415a8c591aba7bedcff1bcf97eb20014e
bytesunite <bytesunite@gmail.com>
2025-06-28 14:16:53 -0700

    building a form

--- ExerciseFiles/Ch06/next-react-app/src/app/contact/page.js
+++ ExerciseFiles/Ch06/next-react-app/src/app/contact/page.js
@@ -1,5 +1,23 @@
 export default function Page() {
   return (
-    <h2>Contact</h2>
+    <main className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md">
+      <h1 className="text-2xl font-bold text-center mb-6 text-black">Contact</h1>
+      <form className="space-y-4">
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            Email
+          </label>
+          <input id="email" type="email" name="email" className="border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500" required />
+        </div>
+        <div>
+          <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label>
+          <textarea id="message" name="message" rows="4" className="border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500" required>
+          </textarea>
+        </div>
+        <button type="submit" className="text-white bg-blue-600 rounded-md p-3">
+          Send Message
+        </button>
+      </form>
+    </main>
   );
 }
